676c60b7cb2b9c0468584f7e0163ebbbe2e4271e
liyinghao <chinaliyinghao@163.com>
2016-10-31 23:05:48 +0800

    jQuery  事件模块

--- 3.event.js
+++ 3.event.js
@@ -1,3 +1,39 @@
 /**
- * Created by liyingjie on 2016/10/30.
+ * Created by liyinghao on 2016/9/26.
  */
+//****************************************
+// 事件操作模块
+//****************************************
+jQuery.fn.extend({
+    on: function ( types, func ) {
+        return this.each(function () {
+            var that = this;
+            types.split( ' ' ).forEach( function ( v, i ) {
+                that.addEventListener( v, func );
+            });
+        });
+    }
+});
+
+
+jQuery.each( [
+    "onabort", "onblur", "oncancel", "oncanplay", "oncanplaythrough", "onchange",
+    "onclick", "onclose", "oncontextmenu", "oncuechange", "ondblclick", "ondrag",
+    "ondragend", "ondragenter", "ondragleave", "ondragover", "ondragstart", "ondrop",
+    "ondurationchange", "onemptied", "onended", "onerror", "onfocus", "oninput",
+    "oninvalid", "onkeydown", "onkeypress", "onkeyup", "onload", "onloadeddata",
+    "onloadedmetadata", "onloadstart", "onmousedown", "onmouseenter", "onmouseleave",
+    "onmousemove", "onmouseout", "onmouseover", "onmouseup", "onmousewheel", "onpause",
+    "onplay", "onplaying", "onprogress", "onratechange", "onreset", "onresize",
+    "onscroll", "onseeked", "onseeking", "onselect", "onshow", "onstalled",
+    "onsubmit", "onsuspend", "ontimeupdate", "ontoggle", "onvolumechange",
+    "onwaiting", "onbeforecopy", "onbeforecut", "onbeforepaste", "oncopy",
+    "oncut", "onpaste", "onsearch", "onselectstart", "onwheel", "onwebkitfullscreenchange",
+    "onwebkitfullscreenerror"], function ( i, v ) {
+    v = v.slice( 2 );
+
+    jQuery.fn[ v ] = function ( eventFn ) {
+        return this.on( v, eventFn );
+    }
+
+});
